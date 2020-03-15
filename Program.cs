b46324649a79bcf879584740bc34808a693a7b21
jinjunhan2000 <62095428+jinjunhan2000@users.noreply.github.com>
2020-03-15 11:56:50 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2_5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manners m = new Manners();
+            try
+            {
+                switch (m.input())
+                {
+                    case "+":
+                        {
+                            m.add();
+                            break;
+                        }
+                    case "-":
+                        {
+                            m.subtraction();
+                            break;
+                        }
+                    case "*":
+                        {
+                            m.multiplication();
+                            break;
+                        }
+                    case "/":
+                        {
+                            m.division();
+                            break;
+                        }
+                }
+                m.Equals();
+            }
+            catch
+            {
+                Console.WriteLine("发生异常，请重启程序");
+            }            
+        }
+    }
+}
